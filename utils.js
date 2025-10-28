b566e0ec37a05f5e9fc366fb8d3bf681203fc5e1
Brandon-Gonsales <bgonsalescoronado@gmail.com>
2025-10-28 18:25:14 -0400

    sacrificar word por pdf prueba

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
-const fs = require('fs/promises');
-//const fs = require('fs');
+//const fs = require('fs/promises');
+const fs = require('fs');
 const path = require('path');
 const mammoth = require("mammoth");
 const xlsx = require('xlsx');
